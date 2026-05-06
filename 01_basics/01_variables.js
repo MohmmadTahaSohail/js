49d340be247238c2a656159aba957e1e2c80410e
MohmmadTahaSohail <mdtahasohailthedevloper@gmail.com>
2026-05-06 11:07:28 +0000

    let done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accId=233455
+let accountEmail="sdfjkfbs@gmail.com"
+var accpass="1234"
+acccity="Hyd"
+
+// accId=2
+
+console.log(accId);
+
+accountEmail="bjshbjsqbj@gmail.com"
+accpass="242434334"
+acccity="Kodad"
+
+
+console.table([accId,accountEmail,accpass,acccity])
